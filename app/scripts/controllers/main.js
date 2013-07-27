0d7c01f7f4a46d5cb5537a9b2c172ff848142820
Tim Nunamaker <tim@timnunamaker.com>
2013-07-26 22:11:20 -0500

    Manage your radar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('techRadarApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', 'radarService', function ($scope, radarService) {
+    $scope.radarData = radarService.radar.data;
+
+    $scope.setActive = function(status) {
+      _.each($scope.radarData, function(status) { status.active = false; });
+      status.active = true;
+    };
+
+    //console.log($scope.radarData[0]);
+    $scope.setActive($scope.radarData[0]);
+
+    $scope.addTech = function(category, tech) {
+        if(tech) {
+            category.technologies.push({label: tech});
+            delete category.new
+        }
+    };
+
+    $scope.removeTech = function(category, tech) {
+        category.technologies = _.without(category.technologies, tech);
+    };
+  }]);
